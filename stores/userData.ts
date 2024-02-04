6437154e1babc39629c390fc3e045a78e401e0e6
C-Valen <charlie.valen@gmail.com>
2024-02-04 16:52:06 +0100

    fix: buffering page zero

--- stores/userData.ts
+++ stores/userData.ts
@@ -32,6 +32,7 @@ export const useUserDataStore = defineStore({
       this.publicRepos = userData.public_repos;
     },
     async loadRepositories() {
+      // Initial repository pages load
       try {
         const appStateStore = useAppStateStore();
         appStateStore.loadingRepos = true;
@@ -46,6 +47,7 @@ export const useUserDataStore = defineStore({
       }
     },
     async loadNextRepositories() {
+      // Takes next page from buffer and prefetchs the next
       try {
         this.prevRepoPage = this.repositories;
         this.repositories = this.nextRepoPage;
@@ -56,16 +58,20 @@ export const useUserDataStore = defineStore({
       }
     },
     async loadPrevRepositories() {
+      // Takes previous page from buffer and prefetchs the previous (if needed)
       try {
         this.nextRepoPage = this.repositories;
         this.repositories = this.prevRepoPage;
-        const newRepositories = await searchRepos(this.login, this.repoPage-1, this.perPage);
-        this.prevRepoPage = this.mapRepositories(newRepositories);
+        if (this.repoPage - 1 > 0) {
+          const newRepositories = await searchRepos(this.login, this.repoPage - 1, this.perPage);
+          this.prevRepoPage = this.mapRepositories(newRepositories);
+        }
       } catch (error) {
         console.error(error.message);
       }
     },
     mapRepositories(repositories: Repository[]): Repository[] {
+      // Maps input into required repository structure
       return repositories.map(({ name, forks, stargazers_count }) => ({
         name,
         forks,
@@ -76,6 +82,7 @@ export const useUserDataStore = defineStore({
       return Math.ceil(this.publicRepos / this.perPage);
     },
     loadPreviousPage() {
+      // Makes previous repository page current and buffers one more previous
       if (this.repoPage > 1) {
         const appStateStore = useAppStateStore();
         appStateStore.bufferingRepos = true;
@@ -85,6 +92,7 @@ export const useUserDataStore = defineStore({
       }
     },
     loadNextPage() {
+      // Makes next repository page current and buffers the next one
       if (this.repoPage < this.calculateTotalPages()) {
         const appStateStore = useAppStateStore();
         appStateStore.bufferingRepos = true;
